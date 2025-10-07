b98531b6152974c8ce8e27e80333fa655c06709b
Brian Okutu <63650953+Ryan10-coder@users.noreply.github.com>
2025-10-07 09:18:19 +0300

    Update CoordinatorPortal.tsx

--- src/pages/CoordinatorPortal.tsx
+++ src/pages/CoordinatorPortal.tsx
@@ -1148,7 +1148,7 @@ const CoordinatorPortal = () => {
                   >
                     {sendingMessage ? (
                       <>
-                        <Loader2 className="h-4 w-4 mr-2 animate-spin" />
+                        <Loader2 className="h-4 w-4 mr-2 animate-spin" /> 
                         Sending...
                       </>
                     ) : (
