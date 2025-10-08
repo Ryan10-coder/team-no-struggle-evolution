9d21e5e61fc8c1bd2a01ab843ed3fb325488666e
Brian Okutu <63650953+Ryan10-coder@users.noreply.github.com>
2025-10-08 10:00:02 +0300

    Update AdminPortal.tsx

--- src/pages/AdminPortal.tsx
+++ src/pages/AdminPortal.tsx
@@ -3097,7 +3097,7 @@ const AdminPortal = () => {
               >
                 {isDeleting ? (
                   <>
-                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin" /> 
                     Deleting...
                   </>
                 ) : (
